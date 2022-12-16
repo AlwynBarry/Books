736e020cef0769d7a36a01d291cd49f15141f30c
Alwyn Barry <alwyn_barry@yahoo.co.uk>
2022-12-16 16:36:53 +0000

    Move the edit link back into the book block
    Rather than rely on floats, put the edit link back into the book block so that all details relating to a book are held together logically.  Rely on proper use of flex css to place the edit link appropriately.
    
    Fix the tabbing so that only spaces are used.

--- books/books-shortcode.php
+++ books/books-shortcode.php
@@ -3,7 +3,7 @@
 
 // No direct calls to this script
 if ( strpos($_SERVER['PHP_SELF'], basename(__FILE__) )) {
-	die('No direct calls allowed!');
+  die('No direct calls allowed!');
 }
 
 
@@ -18,70 +18,70 @@ if ( strpos($_SERVER['PHP_SELF'], basename(__FILE__) )) {
  */
 function books_shortcode( $atts ) {
 
-	$shortcode_atts = shortcode_atts( array( 'posts_per_page' => -1 ), $atts );
-	$posts_per_page = (int) $shortcode_atts['posts_per_page'];
-	if ( $posts_per_page === -1 ) {
-		$nopaging = true;
-	} else {
-		$nopaging = false;
-	}
+  $shortcode_atts = shortcode_atts( array( 'posts_per_page' => -1 ), $atts );
+  $posts_per_page = (int) $shortcode_atts['posts_per_page'];
+  if ( $posts_per_page === -1 ) {
+    $nopaging = true;
+  } else {
+    $nopaging = false;
+  }
 
-	$output = '';
+  $output = '';
 
-	$tax_query = array();
-	if ( ! empty( $atts['category'] ) ) {
-		$cat_in = explode( ',', $atts['category'] );
-		if ( ! empty( $cat_in ) ) {
-			$tax_query['relation'] = 'OR';
-			$tax_query[] = array(
-				'taxonomy'         => 'book_category',
-				'terms'            => $cat_in,
-				'field'            => 'slug',
-				'include_children' => true,
-			);
-		}
-	}
+  $tax_query = array();
+  if ( ! empty( $atts['category'] ) ) {
+    $cat_in = explode( ',', $atts['category'] );
+    if ( ! empty( $cat_in ) ) {
+      $tax_query['relation'] = 'OR';
+      $tax_query[] = array(
+        'taxonomy'         => 'book_category',
+        'terms'            => $cat_in,
+        'field'            => 'slug',
+        'include_children' => true,
+      );
+    }
+  }
 
-	$args = array(
-		'post_type'      => 'books',
-		'post_status'    => 'publish',
-		'posts_per_page' => $posts_per_page,
-		'nopaging'       => $nopaging,
-		'orderby'        => 'name',
-		'order'          => 'ASC',
-		'tax_query'      => $tax_query,
-	);
+  $args = array(
+    'post_type'      => 'books',
+    'post_status'    => 'publish',
+    'posts_per_page' => $posts_per_page,
+    'nopaging'       => $nopaging,
+    'orderby'        => 'name',
+    'order'          => 'ASC',
+    'tax_query'      => $tax_query,
+  );
 
-	// The Query
-	$the_query = new WP_Query( $args );
+  // The Query
+  $the_query = new WP_Query( $args );
 
-	// The Loop
-	if ( $the_query->have_posts() ) {
-		$output .= '
-		<div class="books">
-		';
+  // The Loop
+  if ( $the_query->have_posts() ) {
+    $output .= '
+    <div class="books">
+    ';
 
-		while ( $the_query->have_posts() ) {
-			$the_query->the_post();
+    while ( $the_query->have_posts() ) {
+      $the_query->the_post();
 
       /* Output all the book details */
-			$classes = books_get_term_classes( get_the_ID() );
-			$output .= '
-			<div class="book ' . $classes . '">
+      $classes = books_get_term_classes( get_the_ID() );
+      $output .= '
+      <div class="book ' . $classes . '">
       ';
       /* Output the book image - first try the image URL, and if not available try the Thumbnail */
       $image_url = get_post_meta( get_the_ID(), 'book_image_link', true );
       if ( empty( $image_url ) ) {
         $image_url = get_the_post_thumbnail_url( get_the_ID() );
       }
-			$output .= '      
+      $output .= '      
         <div class="book-image-container"><img class="book-image" src="' . $image_url . '"></div><!-- book-image-container -->
       ';
-			$output .= '      
+      $output .= '      
         <div class="book-details-container">
       ';
       /* Output the book title, with the URL link if provided */
-			$output .= '      
+      $output .= '      
           <div class="book-title">';
       $url = get_post_meta( get_the_ID(), 'book_link', true );
       if ($url) {
@@ -89,7 +89,7 @@ function books_shortcode( $atts ) {
       } else {
         $output .= get_the_title();
       }
-			$output .= '</div><!-- book-title -->
+      $output .= '</div><!-- book-title -->
       ';
 
       /* Output the book description */
@@ -154,37 +154,37 @@ function books_shortcode( $atts ) {
         </div><!-- book-details-container -->
       ';
 
-      $output .= '
-      </div><!--book ' . $classes . ' -->
-      ';
-
       /* Output the edit link if the user is logged in */
-			$postlink = '';
-			if ( is_user_logged_in() ) {
-				$postlink = get_edit_post_link( get_the_ID() );
-				if ( strlen( $postlink ) > 0 ) {
+      $postlink = '';
+      if ( is_user_logged_in() ) {
+        $postlink = get_edit_post_link( get_the_ID() );
+        if ( strlen( $postlink ) > 0 ) {
           $output .= '
           <div class="book-edit-link">
             <a class="post-edit-link" href="' . esc_attr( $postlink ) . '">' . esc_html__('Edit', 'books') . '</a>
           </div><!-- book-edit-link -->
           ';
-				}
+        }
       }
 
-		}
+      $output .= '
+      </div><!--book ' . $classes . ' -->
+      ';
 
-		$output .= '
-		</div><!-- books -->
-		';
+    }
 
-	} else {
-		// no posts found
-		esc_html__( 'No books found', 'books' );
-	}
-	/* Restore original Post Data */
-	wp_reset_postdata();
+    $output .= '
+    </div><!-- books -->
+    ';
 
-	return $output;
+  } else {
+    // no posts found
+    esc_html__( 'No books found', 'books' );
+  }
+  /* Restore original Post Data */
+  wp_reset_postdata();
+
+  return $output;
 
 }
 add_shortcode( 'books', 'books_shortcode' );
